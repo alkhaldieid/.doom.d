6498338d1f76c78a7f181a2532dc36b53bb0882f
Eid Alkhaldi <eid.alkhaldi@gmail.com>
2021-04-06 21:23:37 -0400

    Tue Apr  6 09:23:37 PM EDT 2021

--- init.el
+++ init.el
@@ -148,7 +148,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org  +brain + pandoc +pomodoro +journal +present +jupyter +pretty)     ; organize your plain life in plain text
+       (org  +brain + pandoc +pomodoro +journal +present +jupyter +pretty +roam)     ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
